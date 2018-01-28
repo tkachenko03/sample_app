03168d9c6ceec9f004bed6c456ba9cda9c4d4b0f
Alexandr Tkachenko <a.w.tkachenko@gmail.com>
2018-01-29 00:35:46 +0300

    Add test about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -9,6 +10,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+  end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response 
   end
 
 end
