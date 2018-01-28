ae7d705bfa3e3264e4fb79c93cb0c1dec71c1d1a
Unknown <a.w.tkachenko@gmail.com>
2018-01-29 02:23:30 +0300

    Refactoring static_pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "приложение-пример Ruby on Rails Tutorial"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Главная | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Помощь | #{@base_title}"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
+    assert_select "title", "О программе | #{@base_title}"
   end
 
 end
