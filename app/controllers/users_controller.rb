303a486f114acb6ef9ac421e44b13e5e3ff529fc
Unknown <a.w.tkachenko@gmail.com>
2018-02-10 09:50:08 +0300

    7.4.2 The flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Добро пожаловать в Приложение-пример!"
+      redirect_to @user
     else
       render 'new'
   	end
@@ -24,5 +25,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
   end
-  
+
 end
