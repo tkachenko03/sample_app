0117c1c682e56ba4a3896277ff0a39c36f47738f
Unknown <a.w.tkachenko@gmail.com>
2018-02-10 09:11:39 +0300

    7.3.4 Тест на недопустимое представление

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+# Вывод шаблона сообщений об ошибках 
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
+# Маршрут отправки данных регистрации
+    assert_select  'form[action="/signup"]'
+
+  end
+end
