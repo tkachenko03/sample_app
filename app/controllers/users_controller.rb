59182650459c1740a58de5affccf65878a7d33bc
Unknown <a.w.tkachenko@gmail.com>
2018-02-11 01:33:13 +0300

    8.2.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Добро пожаловать в Приложение-пример!"
       redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -28,6 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     # Наличие вспышки
     assert_not flash.empty?
   end
