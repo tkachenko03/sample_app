3cb1d9330617d05b03c6c19ef5b115dcd2cc07d2
Unknown <a.w.tkachenko@gmail.com>
2018-02-19 00:09:11 +0300

    11.2.4 Изменение действия create users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Добро пожаловать в пример приложения!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Пожалуйста проверьте свой email для активации учетной записи."
+      redirect_to root_url
     else
       render 'new'
   	end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,8 +27,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert is_logged_in?
     # Наличие вспышки
     assert_not flash.empty?
   end
