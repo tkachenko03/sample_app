8a3a5252703fe5f2ab56385442bc30614615458c
Unknown <a.w.tkachenko@gmail.com>
2018-02-19 00:23:01 +0300

    11.3.2 Действие активации edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Учетная запись не активирована. "
+        message += "Проверьте свой email для перехода по ссылке активации."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Не верная комбинация email/пароль' # Not quite right!
       render 'new'
     end
   end
