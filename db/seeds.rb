df3baefc8ebca665a415cc4265cfa9216cb03dc1
Unknown <a.w.tkachenko@gmail.com>
2018-02-18 21:41:42 +0300

    11.1.2 Модель данных активации

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,10 @@ User.create!(name:  "Ткаченко Александр",
              email: "a.w.tkachenko@gmail.com",
              password:              "333221",
              password_confirmation: "333221",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 99.times do |n|
   name  = Faker::Name.name
@@ -18,5 +21,8 @@ User.create!(name:  "Ткаченко Александр",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+
 end
