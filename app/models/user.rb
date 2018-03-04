209f0ddb01c91160e93747b138d44ff5095743e9
Unknown <a.w.tkachenko@gmail.com>
2018-03-05 00:52:26 +0300

    12.3.2 Updating the reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
    # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
@@ -64,6 +64,11 @@ class User < ApplicationRecord
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
