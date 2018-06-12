5038742c54a06e635cb7a6876d7e0d5dffa1f6f4
Unknown <a.w.tkachenko@gmail.com>
2018-06-13 01:58:09 +0300

    14.3.2 A first feed implementation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,9 +7,9 @@ class UserTest < ActiveSupport::TestCase
                    password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should be valid" do 
+  test "should be valid" do
 	assert @user.valid?
-  end	
+  end
 
   test "name should be present" do
   	@user.name = ""
@@ -67,6 +67,24 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
   end
 
 end
